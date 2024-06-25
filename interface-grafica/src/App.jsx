8319a52257bc0932a3095021d325a2ebefa94499
Italo Araujo <italojsouza@gmail.com>
2024-06-25 16:47:53 -0300

    fix: graphic interface completely functional

--- interface-grafica/src/App.jsx
+++ interface-grafica/src/App.jsx
@@ -5,8 +5,9 @@ import axios from 'axios'
 const URL = 'http://localhost:8080/';
 
 const defaultState = {
-  isDisabled: true,
-  labelText: 'Informação adicional:',
+  inputDisabled: true,
+  buttonDisabled: true,
+  labelText: '...',
   endOfUrl: '',
   reponseData: '...',
   inputValue: '',
@@ -15,25 +16,79 @@ const defaultState = {
 function App() {
   const [state, setState] = useState(defaultState)
 
-  const handleVerTodosClick = async () => {
+  const doGetRequest = async (endOfUrl) => {
     try {
-      const response = await axios.get(URL)
-      setState({ ...state, isDisabled: true, reponseData: JSON.stringify(response.data), labelText: 'Informação adicional:'})
+      const response = await axios.get(URL+endOfUrl)
+      setState({ ...state, inputDisabled: true, buttonDisabled: true, reponseData: JSON.stringify(response.data), labelText: defaultState.labelText})
     } catch (error) {
-      setState({ ...state, isDisabled: true, reponseData: error.message, labelText: 'Informação adicional:'})
+      setState({ ...state, inputDisabled: true, buttonDisabled: true, reponseData: error.message, labelText: defaultState.labelText})
     }
   }
 
   const handleDeleteClick = async () => {        
-      setState({ ...state, isDisabled: false, labelText: 'Informe o nome do funcionário a ser excluído:', endOfUrl: 'delete/' })
+    setState({ ...state, inputDisabled: false, buttonDisabled: false, labelText: 'Informe o nome do funcionário a ser excluído e confirme:', endOfUrl: 'delete/' })
+  }
+
+  const handleAumentarSalarioClick = async () => {
+    setState({ ...state, inputDisabled: true, buttonDisabled: false, labelText: 'Confirme aumento de salário:', endOfUrl: 'aumenta-salario' })
+  }
+
+  const handleAniversariosClick = async () => {
+    setState({ ...state, inputDisabled: false, buttonDisabled: false, labelText: 'Insira os meses (números, separados por vírgula) e confirme:', endOfUrl: 'aniversario/?months=' })
+  }
+
+  const handleVerTodosClick = async () => {
+    await doGetRequest('')
+  }
+
+  const handleAgruparPorFuncaoClick = async () => {
+    await doGetRequest('funcoes')
+  }
+
+  const handleMaisVelhoClick = async () => {
+    await doGetRequest('mais-velho')
+  }
+
+  const handleOrdemAlfabeticaClick = async () => {
+    await doGetRequest('ordenada')
+  }
+
+  const handleTotalSalariosClick = async () => {
+    await doGetRequest('total')
+  }
+
+  const handleSalariosMinimosClick = async () => {
+    await doGetRequest('salarios-min')
   }
 
   const handleConfirm = async () => {
-    try {
-      await axios.delete(URL+state.endOfUrl+state.inputValue)
-      setState({ ...state, isDisabled: true, reponseData: 'Funcionário excluído com sucesso!' })
-    } catch (error) {
-      setState({ ...state, isDisabled: true, reponseData: error.message })
+    switch (state.endOfUrl) {
+      case 'delete/':
+        try {
+          await axios.delete(URL+state.endOfUrl+state.inputValue)
+          setState({ ...state, isDisabled: true, reponseData: 'Funcionário excluído com sucesso!', endOfUrl: ''})
+        } catch (error) {
+          setState({ ...state, isDisabled: true, reponseData: error.message, endOfUrl: '' })
+        }
+        break;
+      case 'aumenta-salario':
+        try {
+          await axios.patch(URL+state.endOfUrl)
+          setState({ ...state, isDisabled: true, reponseData: 'Salários aumentados com sucesso!', endOfUrl: ''})
+        } catch (error) {
+          setState({ ...state, isDisabled: true, reponseData: error.message, endOfUrl: '' })
+        }
+        break;
+      case 'aniversario/?months=':
+        try {
+          const response = await axios.get(URL+state.endOfUrl+state.inputValue)
+          setState({ ...state, inputDisabled: true, buttonDisabled: true, reponseData: JSON.stringify(response.data), labelText: 'Aniversariantes:', endOfUrl: ''})
+        } catch (error) {
+          setState({ ...state, inputDisabled: true, buttonDisabled: true, reponseData: error.message, labelText: defaultState.labelText, endOfUrl: ''})
+        }
+        break;
+      default:
+        break;
     }
   }
 
@@ -51,31 +106,31 @@ function App() {
         <button onClick={handleDeleteClick}>
           Deletar
         </button>
-        <button>
+        <button onClick={handleAumentarSalarioClick}>
           Aumentar salário(10%)
         </button>
-        <button>
+        <button onClick={handleAgruparPorFuncaoClick}>
           Agrupar por função
         </button>
-        <button>
+        <button onClick={handleAniversariosClick}>
           Aniversários
         </button>
-        <button>
+        <button onClick={handleMaisVelhoClick}>
           Mostrar mais velho
         </button>
-        <button>
+        <button onClick={handleOrdemAlfabeticaClick}>
           Ver todos em ordem alfabética
         </button>
-        <button>
+        <button onClick={handleTotalSalariosClick}>
           Total dos salários
         </button>
-        <button>
+        <button onClick={handleSalariosMinimosClick}>
           Salários Mínimos
         </button>
       </div>
       <label htmlFor="myInput">{state.labelText}
-        <input id="myInput" type="text" disabled={state.isDisabled} onChange={handleInputChange}/>
-        <button disabled={state.isDisabled} onClick={handleConfirm}>Confirmar</button>
+        <input id="myInput" type="text" disabled={state.inputDisabled} onChange={handleInputChange}/>
+        <button disabled={state.buttonDisabled} onClick={handleConfirm}>Confirmar</button>
       </label>      
       <div className="exibicao">
         {state.reponseData}    
